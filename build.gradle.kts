dda2314471d362f4f82bb353b6332c98ec3e82e5
Nikita Klyshko <n-klyshko@mail.ru>
2023-09-02 13:05:47 +0300

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,54 @@
+import org.gradle.configurationcache.extensions.capitalized
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+group = "com.example" // TODO: Change this to your group
+version = "1.0-SNAPSHOT" // TODO: Change this to your addon version
+
+val mojangMapped = project.hasProperty("mojang-mapped")
+
+plugins {
+    alias(libs.plugins.kotlin)
+    alias(libs.plugins.nova)
+    alias(libs.plugins.stringremapper)
+    alias(libs.plugins.specialsource)
+}
+
+dependencies {
+    implementation(libs.nova)
+}
+
+addon {
+    id.set(project.name)
+    name.set(project.name.capitalized())
+    version.set(project.version.toString())
+    novaVersion.set(libs.versions.nova)
+    main.set("com.example.ExampleAddon") // TODO: Change this to your main class
+    authors.add("ExampleAuthor") // TODO: Set your list of authors
+}
+
+spigotRemap {
+    spigotVersion.set(libs.versions.spigot.get().substringBefore('-'))
+    sourceJarTask.set(tasks.jar)
+}
+
+remapStrings {
+    remapGoal.set(if (mojangMapped) "mojang" else "spigot")
+    spigotVersion.set(libs.versions.spigot.get())
+}
+
+tasks {
+    register<Copy>("addonJar") {
+        group = "build"
+        dependsOn("addon", if (mojangMapped) "jar" else "remapObfToSpigot")
+        
+        from(File(project.buildDir, "libs/${project.name}-${project.version}.jar"))
+        into((project.findProperty("outDir") as? String)?.let(::File) ?: project.buildDir)
+        rename { it.replace(project.name, addon.get().addonName.get()) }
+    }
+    
+    withType<KotlinCompile> {
+        kotlinOptions {
+            jvmTarget = "17"
+        }
+    }
+}
