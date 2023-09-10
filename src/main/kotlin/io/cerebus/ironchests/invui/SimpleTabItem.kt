6353b3fd04b89e8f98063287aaf818119a6af2cb
Nikita Klyshko <n-klyshko@mail.ru>
2023-09-10 13:06:10 +0300

    Closes #1 Add chest tab switch click sound

--- src/main/kotlin/io/cerebus/ironchests/invui/SimpleTabItem.kt
+++ src/main/kotlin/io/cerebus/ironchests/invui/SimpleTabItem.kt
@@ -1,9 +1,13 @@
 package io.cerebus.ironchests.invui
 
+import org.bukkit.entity.Player
+import org.bukkit.event.inventory.ClickType
+import org.bukkit.event.inventory.InventoryClickEvent
 import xyz.xenondevs.invui.gui.TabGui
 import xyz.xenondevs.invui.item.ItemProvider
 import xyz.xenondevs.invui.item.builder.ItemBuilder
 import xyz.xenondevs.invui.item.impl.controlitem.TabItem
+import xyz.xenondevs.nova.util.playClickSound
 
 class SimpleTabItem(private val tab: Int, private val activeItem: ItemBuilder, private val inactiveItem: ItemBuilder) : TabItem(tab) {
     
@@ -11,4 +15,8 @@ class SimpleTabItem(private val tab: Int, private val activeItem: ItemBuilder, p
         return if (gui.currentTab == tab) activeItem else inactiveItem
     }
     
+    override fun handleClick(clickType: ClickType, player: Player, event: InventoryClickEvent) {
+        super.handleClick(clickType, player, event)
+        player.playClickSound()
+    }
 }
