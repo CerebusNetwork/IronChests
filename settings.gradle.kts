9c006a8080d554f3f0c333dc6439b9fbd1aa8a40
Nikita Klyshko <n-klyshko@mail.ru>
2023-10-27 20:16:46 +0300

    Upgrade to Nova 0.15-alpha.5

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,7 +8,7 @@ dependencyResolutionManagement {
     }
     versionCatalogs {
         create("libs") {
-            from("xyz.xenondevs.nova:catalog:0.15-alpha.4")
+            from("xyz.xenondevs.nova:catalog:0.15-alpha.5")
         }
     }
 }
