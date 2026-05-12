791907da502f68faba131718def49ad271473615
Nikita Klyshko <n-klyshko@mail.ru>
2026-05-12 09:56:59 +0300

    Fix incorrect 'upgrading' iron, golden, diamond chests to iron chest with WoodChestUpgrade

--- src/main/kotlin/io/cerebus/ironchests/item/WoodChestUpgradeBehavior.kt
+++ src/main/kotlin/io/cerebus/ironchests/item/WoodChestUpgradeBehavior.kt
@@ -10,13 +10,14 @@ import org.bukkit.inventory.BlockInventoryHolder
 import org.bukkit.inventory.DoubleChestInventory
 import org.bukkit.inventory.ItemStack
 import xyz.xenondevs.invui.inventory.event.UpdateReason
+import xyz.xenondevs.nova.util.novaBlock
 import xyz.xenondevs.nova.world.format.WorldDataManager
 import xyz.xenondevs.nova.world.pos
 import org.bukkit.block.data.type.Chest as ChestBlockData
 
 object WoodChestUpgradeBehavior : BaseUpgradeBehaviour(Blocks.IRON_CHEST) {
 
-    override fun isValidTargetBlock(block: Block): Boolean = (block.type == Material.CHEST)
+    override fun isValidTargetBlock(block: Block): Boolean = (block.type == Material.CHEST && block.novaBlock == null)
     
     override fun getOriginalChestData(block: Block): ChestData {
         val chestInventory = (block.world.getBlockState(block.location) as Chest).inventory
